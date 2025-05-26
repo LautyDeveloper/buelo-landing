ed0034e74af3735e01d2fa4c995d1a8820c5d8f9
Lautaro Aquino <laufabio24@gmail.com>
2025-05-26 15:41:03 -0300

    Desarrollo final de las cartas de About

--- src/views/about-us/components/card.jsx
+++ src/views/about-us/components/card.jsx
@@ -3,8 +3,13 @@ import "./card.css";
 export default function Card({ data }) {
   return (
     <div className="about-us-card">
-      <div className="card-icon-container">{data.icon}</div>
-      <strong>{data.title}</strong>
+      <div
+        className="card-icon-container"
+        style={{ backgroundColor: data.bgColor }}
+      >
+        {data.icon}
+      </div>
+      <strong style={{ color: data.color }}>{data.title}</strong>
       <p>{data.text}</p>
     </div>
   );
